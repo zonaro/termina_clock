16e854a7b2f1e4c13f62721d544171ccdbf93480
Kaizonaro <zonaro@outlook.com>
2024-11-06 04:25:06 -0300

    adiciona controle deslizante e atualiza o estado do TerminaClock

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:termina_clock/termina_clock.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  double _sliderValue = 0;
+
+  final GlobalKey<TerminaClockState> _terminaClockKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: TerminaClock(size: 200),
-        ),
+            child: ListView(
+          children: [
+            TerminaClock(
+              key: _terminaClockKey,
+              dateTime: DateTime(2024),
+              staticClock: true,
+            ),
+            Text(_terminaClockKey.currentState?.dateTime.toString() ?? ""),
+            Slider(
+              value: _sliderValue,
+              min: -1 * (60 * 24),
+              max: 60 * 24,
+              label: _sliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sliderValue = value;
+                  _terminaClockKey.currentState?.dateTime = DateTime(2024).add(Duration(minutes: _sliderValue.toInt()));
+                });
+              },
+            ),
+          ],
+        )),
       ),
     );
   }
--- lib/termina_clock.dart
+++ lib/termina_clock.dart
@@ -5,31 +5,41 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class TerminaClock extends StatefulWidget {
-  final double size;
+  final double? size;
   final DateTime? dateTime;
   final bool staticClock;
 
-  const TerminaClock({super.key, this.size = 200, this.dateTime, this.staticClock = false});
+  const TerminaClock({super.key, this.size, this.dateTime, this.staticClock = false});
 
   @override
-  createState() => _TerminaClockState();
+  createState() => TerminaClockState();
 }
 
-class _TerminaClockState extends State<TerminaClock> {
+class TerminaClockState extends State<TerminaClock> {
   Timer? _timer;
   late DateTime _currentTime;
+
+  DateTime get dateTime => _currentTime;
+  set dateTime(DateTime dateTime) {
+    setState(() {
+      _currentTime = dateTime;
+    });
+  }
 
   String get _center => "assets/termina_clock/center.png";
   String get _dayIndicator => "assets/termina_clock/day_indicator.png";
   String get _hourHand => "assets/termina_clock/hours_disc.png";
   String get _indicator => _currentTime.isAfter(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 5, 59, 59)) && _currentTime.isBefore(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 18)) ? _dayIndicator : _nightIndicator;
   String get _minuteHand => "assets/termina_clock/minutes_disc.png";
+
   String get _nightIndicator => "assets/termina_clock/night_indicator.png";
 
   @override
   Widget build(BuildContext context) {
     double minuteRotation = (_currentTime.minute / 60) + (_currentTime.second / 3600);
     double hourRotation = (_currentTime.hour / 12) + (_currentTime.minute / 720);
+
+    double size = widget.size ?? MediaQuery.sizeOf(context).width;
 
     return SizedBox(
       width: widget.size,
@@ -45,24 +55,25 @@ class _TerminaClockState extends State<TerminaClock> {
             child: Stack(
               alignment: Alignment.center,
               children: [
-                SizedBox(width: widget.size * .75, child: Image.asset(_hourHand, package: "termina_clock")),
+                SizedBox(width: size * .75, child: Image.asset(_hourHand, package: "termina_clock")),
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
                   children: [
                     SizedBox(
-                      height: widget.size * .48,
+                      height: size * .48,
                     ),
                     SizedBox(
-                      width: widget.size * .25,
-                      child: FittedBox(
-                        child: Image.asset(_indicator, package: "termina_clock"),
-                      ),
+                      width: size * .25,
+                      child: Image.asset(_indicator, package: "termina_clock"),
                     ),
                   ],
                 ),
               ],
             ), // Hour Hand
           ),
-          SizedBox(width: widget.size * .15, child: Center(child: Image.asset(_center, package: "termina_clock"))), // Center
+          SizedBox(width: size * .15, child: Center(child: Image.asset(_center, package: "termina_clock"))), // Center
         ],
       ),
     );
