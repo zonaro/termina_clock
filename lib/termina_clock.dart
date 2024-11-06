6ad58181455aafbbad97b1dd8e2e1b0091f5d9e1
Kaizonaro <zonaro@outlook.com>
2024-11-06 04:59:24 -0300

    adiciona suporte a personalização do mostrador no TerminaClock

--- lib/termina_clock.dart
+++ lib/termina_clock.dart
@@ -4,18 +4,40 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+class ClockFace {
+  final Image? center;
+  final Image? dayIndicator;
+  final Image hourHand;
+  final Image minuteHand;
+  final Image? nightIndicator;
+  final Image? secondsHand;
+
+  const ClockFace({this.center, this.dayIndicator, required this.hourHand, required this.minuteHand, this.nightIndicator, this.secondsHand});
+
+  ClockFace.termina()
+      : center = Image.asset("assets/termina_clock/center.png", package: "termina_clock"),
+        secondsHand = null,
+        dayIndicator = Image.asset("assets/termina_clock/day_indicator.png", package: "termina_clock"),
+        hourHand = Image.asset("assets/termina_clock/hours_disc.png", package: "termina_clock"),
+        minuteHand = Image.asset("assets/termina_clock/minutes_disc.png", package: "termina_clock"),
+        nightIndicator = Image.asset("assets/termina_clock/night_indicator.png", package: "termina_clock");
+}
+
 class TerminaClock extends StatefulWidget {
-  final double? size;
   final DateTime? dateTime;
   final bool staticClock;
+  final ClockFace? face;
 
-  const TerminaClock({super.key, this.size, this.dateTime, this.staticClock = false});
+  const TerminaClock({super.key, this.dateTime, this.staticClock = false}) : face = null;
+  const TerminaClock.custom({super.key, this.dateTime, this.staticClock = false, required this.face});
 
   @override
   createState() => TerminaClockState();
 }
 
 class TerminaClockState extends State<TerminaClock> {
+  late ClockFace _faces;
+
   Timer? _timer;
   late DateTime _currentTime;
 
@@ -26,43 +48,48 @@ class TerminaClockState extends State<TerminaClock> {
     });
   }
 
-  String get _center => "assets/termina_clock/center.png";
-  String get _dayIndicator => "assets/termina_clock/day_indicator.png";
-  String get _hourHand => "assets/termina_clock/hours_disc.png";
-  String get _indicator => _currentTime.isAfter(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 5, 59, 59)) && _currentTime.isBefore(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 18)) ? _dayIndicator : _nightIndicator;
-  String get _minuteHand => "assets/termina_clock/minutes_disc.png";
-
-  String get _nightIndicator => "assets/termina_clock/night_indicator.png";
+  Image? get _indicator {
+    if (_faces.dayIndicator != null || _faces.nightIndicator != null) {
+      return _currentTime.isAfter(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 5, 59, 59)) && _currentTime.isBefore(DateTime(_currentTime.year, _currentTime.month, _currentTime.day, 18)) ? _faces.dayIndicator : _faces.nightIndicator;
+    }
+    return _faces.dayIndicator ?? _faces.nightIndicator;
+  }
 
   @override
   Widget build(BuildContext context) {
     double minuteRotation = (_currentTime.minute / 60) + (_currentTime.second / 3600);
     double hourRotation = (_currentTime.hour / 12) + (_currentTime.minute / 720);
+    double secondRotation = _currentTime.second / 60;
 
-    double size = widget.size ?? MediaQuery.sizeOf(context).width;
-
-    return SizedBox(
-      width: size,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          RotationTransition(
-            turns: AlwaysStoppedAnimation(minuteRotation),
-            child: Image.asset(_minuteHand, package: "termina_clock"), // Minute Hand
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        RotationTransition(
+          turns: AlwaysStoppedAnimation(minuteRotation),
+          child: _faces.minuteHand, // Minute Hand
+        ),
+        RotationTransition(
+          turns: AlwaysStoppedAnimation(hourRotation),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              _faces.hourHand, // Hour Hand
+              if (_indicator != null) _indicator! // Day/Night Indicator
+            ],
           ),
+        ),
+        if (_faces.secondsHand != null)
           RotationTransition(
-            turns: AlwaysStoppedAnimation(hourRotation),
+            turns: AlwaysStoppedAnimation(secondRotation),
             child: Stack(
               alignment: Alignment.center,
               children: [
-                Image.asset(_hourHand, package: "termina_clock"),
-                Image.asset(_indicator, package: "termina_clock"),
+                _faces.secondsHand!, // Hour Hand
               ],
-            ), // Hour Hand
+            ),
           ),
-          Center(child: Image.asset(_center, package: "termina_clock")), // Center
-        ],
-      ),
+        if (_faces.center != null) _faces.center!, // Center
+      ],
     );
   }
 
@@ -75,6 +102,7 @@ class TerminaClockState extends State<TerminaClock> {
   @override
   void initState() {
     super.initState();
+    _faces = widget.face ?? ClockFace.termina();
     _currentTime = widget.dateTime ?? DateTime.now();
     if (!widget.staticClock) {
       _timer = Timer.periodic(const Duration(milliseconds: 1), (timer) {
